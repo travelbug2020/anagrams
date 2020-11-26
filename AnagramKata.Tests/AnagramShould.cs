7a89fd7387b5b84bb67d0a000605339f8d15bd1a
Rachel Whitmore <rachel.whitmore@mandg.com>
2020-11-26 21:01:21 +0000

    Added cat

--- AnagramKata.Tests/AnagramShould.cs
+++ AnagramKata.Tests/AnagramShould.cs
@@ -4,19 +4,27 @@ namespace AnagramKata.Tests
 {
     public class AnagramShould
     {
+        private Anagram anagram;
+
         [SetUp]
         public void Setup()
         {
+            var anagram = new Anagram();
         }
 
         [Test]
-        public void ReturnAnagram_WithFirstLetterInSamePosition_WhenGivenDog()
+        public void ReturnAnagram_WithAllPotentialAnagrams_WhenGivenDog()
         {
-            var anagram = new Anagram();
-
             var result = anagram.GenerateAllAnagramsFor("dog");
 
-            Assert.AreEqual(new string[] { "dog","dgo","gdo","god","odg","ogd" }, result);
+            Assert.AreEqual(new[] { "dog","dgo","odg","ogd","gdo","god" }, result);
+        }
+
+        [Test]
+        public void ReturnAnagram_WithAllPotentialAnagrams_WhenGivenCat()
+        {
+            var result = anagram.GenerateAllAnagramsFor("cat");
+            Assert.AreEqual(new[] { "cat", "cta", "atc", "act", "tac", "tca" },result);
         }
     }
 }
